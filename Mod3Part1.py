5aacf9a27fc4462befdccbe96e009a7d71c6fd87
Mikaela Grace <mikaelagrace@users.noreply.github.com>
2022-11-17 20:03:50 -0500

    Update Mod3Part1.py
    No need to merge the Pull Request if you don't want to, it's just a way for me to suggest updates with inline diffs!

--- Mod3Part1.py
+++ Mod3Part1.py
@@ -6,13 +6,25 @@ from scipy.special import softmax
 import csv
 import urllib.request
 
-task='sentiment'
-MODEL = f"cardiffnlp/twitter-roberta-base-{task}"
+# import ordering: https://stackoverflow.com/questions/20762662/whats-the-correct-way-to-sort-python-import-x-and-from-x-import-y-statement
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL)
+
+_TASK='sentiment'
+_MODEL = f"cardiffnlp/twitter-roberta-base-{task}"
+# Naming: https://google.github.io/styleguide/pyguide.html#316-naming and https://www.ceos3c.com/python/python-constants/
+
+
+
+# It’s best practice to have your code in a descriptive method or small class, if possible. 
+# makes it easier for other modules to import the functionality later if needed! 
+
+# Classes: https://www.dataquest.io/blog/using-classes-in-python/
+
+
+tokenizer = AutoTokenizer.from_pretrained(_MODEL)
 
 labels=[]
-mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/{task}/mapping.txt"
+mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/{TASK}/mapping.txt"
 with urllib.request.urlopen(mapping_link) as f:
     html = f.read().decode('utf-8').split("\n")
     csvreader = csv.reader(html, delimiter='\t')
@@ -28,7 +40,7 @@ with open('tiny_movie_reviews_dataset.txt') as f:
       scores = softmax(scores)
       ranking = np.argsort(scores)
       ranking = ranking[::-1]
-      l = labels[ranking[0]]
+      l = labels[ranking[0]] # nit: discouraged to have one-letter variables
       if l == 'neutral':
         print(labels[ranking[1]])
       else:
